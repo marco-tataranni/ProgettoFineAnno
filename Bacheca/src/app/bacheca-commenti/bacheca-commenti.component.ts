709705729efe28819997be7f4679ef09713d694a
marco-tataranni <tataranni.marco@iisgalvanimi.edu.it>
2019-05-24 07:19:46 +0000

    inserimento di un post

--- Bacheca/src/app/bacheca-commenti/bacheca-commenti.component.ts
+++ Bacheca/src/app/bacheca-commenti/bacheca-commenti.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  FormBuilder,FormGroup, Validators} from '@angular/forms';
+import {Post}from '../post';
+import {Posts} from '../mock-post';
 @Component({
   selector: 'app-bacheca-commenti',
   templateUrl: './bacheca-commenti.component.html',
@@ -7,8 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BachecaCommentiComponent implements OnInit {
 
-  constructor() { }
+   myForm: FormGroup;
+   posts=Posts;
 
+    constructor(fb:FormBuilder)
+    {
+      this.myForm=fb.group({
+          'data':['', Validators.required],
+          'titolo':['', Validators.required],
+          'testo':['', Validators.required],
+          'user':['', Validators.required],
+
+
+      })
+    }
+
+     onSubmit(value: string): void {
+
+    let post: Post = new Post();
+    post.dataInsrimento = this.myForm.controls['data'].value;
+    post.titolo = this.myForm.controls['titolo'].value;
+    post.testo = this.myForm.controls['testo'].value;
+    post.userName = this.myForm.controls['user'].value;
+
+
+    this.posts.push(post);
+  }
   ngOnInit() {
   }
 
